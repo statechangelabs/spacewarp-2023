58438ae54323a998a7758cc3e3ce1084332cde13
Ray Deck <ray@raydeck.com>
2023-02-02 14:06:49 -0500

    feat: add tailwind support into app

--- parcel-app/src/App.tsx
+++ parcel-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
 export const App:FC = () => {
-    return <h1>Hello world!</h1>;
+    return <h1 className="text-lg font-md text-red-500">Hello world!</h1>;
   }
--- parcel-app/src/index.tsx
+++ parcel-app/src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-
+import "./index.css";
 const container = document.getElementById("app");
 if(container) {
 const root = createRoot(container)
