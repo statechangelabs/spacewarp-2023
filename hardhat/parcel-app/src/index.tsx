94f89d839dd4de2dc46d2e2d18af6d28e34dd37c
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2023-02-04 21:57:19 -0500

    removed parcel from hardhat

--- hardhat/parcel-app/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-const container = document.getElementById("app");
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
-}
